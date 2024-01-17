2876f464e4846240cf84eafd1355636e911da2ee
Silvio <silvionovasmartinez@gmail.com>
2024-01-17 22:25:15 +0100

    zoom con js y fix

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const item = document.querySelector(".item");
+const items = document.getElementsByClassName("item");
 const header = document.getElementsByTagName("header")[0];
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
-item.addEventListener("click", () => {
-    item.classList.toggle("zoom")
-    header.classList.toggle("hidden")
-})
+
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", () => {
+        items[i].classList.toggle("zoom")
+        header.classList.toggle("hidden")
+    })
+
+}
