08affdc5bfc55908b44b17e7aa9d15cedab6265b
Silvio <silvionovasmartinez@gmail.com>
2024-01-17 21:37:26 +0100

    menu fix and js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const item = document.querySelector(".item");
+const header = document.getElementsByTagName("header")[0];
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+})
+item.addEventListener("click", () => {
+    item.classList.toggle("zoom")
+    header.classList.toggle("hidden")
 })
