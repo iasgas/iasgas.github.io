4304bb459a43c6f4a0f9b31b1ddcfc70b638f3c0
iasgas <tobias.gasser1@gmail.com>
2024-03-21 08:18:48 +0100

    new landingpage

---
+++ copypaste.js
@@ -0,0 +1,7 @@
+const Projects = document.querySelector(".dropdown");
+const menu = document.querySelector(".menu-container");
+
+
+Projects.addEventListener("click", () => {
+    menu.classList.toggle("active")
+})
