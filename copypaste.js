01a3c98de12f20504707b14d771f906eb139ab74
iasgas <tobias.gasser1@gmail.com>
2024-04-10 08:42:52 +0200

    transition

--- copypaste.js
+++ copypaste.js
@@ -1,7 +1,15 @@
 const Projects = document.querySelector(".dropdown");
 const menu = document.querySelector(".menu-container");
+const contact = document.querySelector("#contact");
+const footer = document.querySelector("footer");
+const container = document.querySelector(".page-container");
 
 
 Projects.addEventListener("click", () => {
     menu.classList.toggle("active")
+})
+
+
+contact.addEventListener("click", () => {
+    container.classList.toggle("active")
 })
