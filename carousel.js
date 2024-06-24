81c7231e72f412965df4434a904277fb56076c7f
iasgas <tobias.gasser1@gmail.com>
2024-06-24 18:45:29 +0200

    new landing page

--- carousel.js
+++ carousel.js
@@ -1,14 +1,14 @@
-const header = document.querySelector("header");
-const slideshow = document.querySelector(".Slideshow");
-const Name= document.querySelector(".name");
-const contact = document.querySelector(".contact");
-const contactdrop= document.querySelector(".contact-drop");
-const contactclose= document.querySelector(".contact-close");
+const one = document.querySelectorAll('.one');
+const two = document.querySelectorAll('.two');
 
 
-Name.addEventListener("click", () => {
-    contact.classList.toggle("active")
-    contactdrop.classList.toggle("active") 
-    slideshow.classList.toggle("active")
-    Name.classList.toggle("active")
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length; 
+    showSlide();
+}
+
+//  Initial display
+one.addEventListener("click", () => {
+    two.classList.toggle("active")
 })
+
---
+++ simple.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+const Cont = document.querySelector("#contact");
+
+contact.addEventListener("click", () => {
+    body.classList.toggle("active")
+})
+
