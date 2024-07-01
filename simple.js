e6d058f64147eac85c53b8d787e726cf210b0183
iasgas <tobias.gasser1@gmail.com>
2024-07-01 20:25:02 +0200

    junk

--- simple.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector("body");
-const Cont = document.querySelector("#contact");
-
-contact.addEventListener("click", () => {
-    body.classList.toggle("active")
-})
-
